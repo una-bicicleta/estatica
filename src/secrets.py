8a49c0578a46663d020ad2638a07c116efb59c26
za <zakiakhmad@gmail.com>
2023-09-04 13:54:50 +0700

    Trying security hotspot SAST snippet

---
+++ src/secrets.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from aws_cdk import (
+    aws_apigateway as apigateway
+)
+
+resource = api.root.add_resource("example")
+resource.add_method(
+    "GET",
+    authorization_type=apigateway.AuthorizationType.NONE  # Sensitive
+)
